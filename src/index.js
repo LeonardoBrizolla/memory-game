ab46458d4b6389d40b54d03cda708943bfa03bb8
LeonardoBrizolla <leonardobrizollars@gmail.com>
2021-12-27 01:34:11 -0300

    Class 21 - Challenge done :D

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const $root = document.querySelector('#root');
 const $memoryCard = document.createElement('article');
-const $icon = `<img src="img/icon-collabcode.svg"
-alt="Gueio mascote da CollabCode" class="icon"/>`;
+const $memoryCardVerse = document.createElement('article');
+const $icon = `
+  <img
+    src="img/icon-collabcode.svg"
+    alt="Gueio mascote da CollabCode"
+    class="icon"
+  />
+`;
+const $iconJS = `
+  <img
+    src="img/icon-js.png"
+    alt="Verso do card"
+    class="icon"
+  />
+`;
 
 $root.insertBefore($memoryCard, null);
-
 $memoryCard.classList.add('memory-card');
 $memoryCard.insertAdjacentHTML('afterbegin', $icon);
+
+$root.insertBefore($memoryCardVerse, null);
+$memoryCardVerse.classList.add('memory-card');
+$memoryCardVerse.classList.add('-verse');
+$memoryCardVerse.insertAdjacentHTML('afterbegin', $iconJS);
