078bef67b0fa92c9fc933915f118f36ff5a5ec90
LeonardoBrizolla <leonardobrizollars@gmail.com>
2021-12-29 16:03:11 -0300

    Component, tudo numa pasta só

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,23 +1,34 @@
 const $root = document.querySelector('#root');
 
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard(
-  'img/icon-collabcode.svg',
-  'Gueio mascote da CollabCode'
-);
-const $memoryCardFront = createMemoryCard(
-  'img/icon-js.png',
-  'Livro JS',
-  '-front'
-);
+const $memoryCardJS = createMemoryCard({
+  nameClass: '-front',
+  src: 'img/icon-js.png',
+  alt: 'Livro JS',
+});
+const $memoryCardBug = createMemoryCard({
+  src: 'img/icon-bug.png',
+  alt: 'Bug',
+  nameClass: '-front',
+});
+const $memoryCardWoman = createMemoryCard({
+  src: 'img/icon-woman.png',
+  alt: 'Woman code',
+  nameClass: '-front',
+});
+const $memoryCardHeadCode = createMemoryCard({
+  src: 'img/icon-head.png',
+  alt: 'Cabeça',
+  nameClass: '-front',
+});
 
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCard);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardBug);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardWoman);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardWoman);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardHeadCode);
+$cardsWrapper.insertAdjacentHTML('beforeend', $memoryCardHeadCode);
 
 $root.insertAdjacentElement('beforeend', $cardsWrapper);
